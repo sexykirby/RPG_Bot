c9896662138e4008d1330fb8b17c41908b6d83f1
sexykirby <jin.db12@gmail.com>
2024-05-12 18:35:38 +0800

    Update bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import sqlite3
+import json
+
+with open('setting.json', 'r', encoding = 'utF8') as jFile:
+    jData = json.load(jFile)
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.messages = True
+bot = commands.Bot(command_prefix = '!', intents = intents)
+
+
+#確認機器人成功啟動
+@bot.event
+async def on_ready():
+    print(">> bot is online <<")
+
+bot.run(jData['TOKEN'])
